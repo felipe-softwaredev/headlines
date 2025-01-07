fc94139cf10ef3cfca6e5bbb97d59f480ab021d7
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2025-01-07 01:28:38 -0500

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: true,
-  //   strictPort: true,
-  //   port: 8000,
-  // },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
